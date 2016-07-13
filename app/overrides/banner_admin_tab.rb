67d3f3df90c0034977c9fa2a00f3114d2d347249
Denise Fugihara <contato@denika.com.br>
2016-07-13 13:17:09 -0300

    get admin link texts from locale

--- app/overrides/banner_admin_tab.rb
+++ app/overrides/banner_admin_tab.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                       :name => "banner_box_admin_tab",
                       :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                      :text => "<ul class='nav nav-sidebar'><%= tab(:banner_boxes, :icon => 'icon icon-bookmark') %></ul>")
+                      :text => "<ul class='nav nav-sidebar'><%= tab t(:banner_boxes), url: admin_banner_boxes_url, icon: 'user' %></ul>")
 
 Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_configuration",
                       :name => "add_banner_box_settings",
                       :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                      :text => "<%= configurations_sidebar_menu_item(:banner_box_settings, edit_admin_banner_box_settings_url) %>")
+                      :text => "<%= configurations_sidebar_menu_item(t(:banner_box_settings), edit_admin_banner_box_settings_url) %>")
