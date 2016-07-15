00cad8bc40c65fa5133b2002343a7cdd1ce5f516
Denise Fugihara <contato@denika.com.br>
2016-07-14 23:27:21 -0300

    spree 3.1.0 admin adaptation

--- app/controllers/spree/admin/banner_boxes_controller.rb
+++ app/controllers/spree/admin/banner_boxes_controller.rb
@@ -1,15 +1,15 @@
 module Spree
   module Admin
     class BannerBoxesController < ResourceController
-      
+
       def index
         respond_with(@collection)
       end
-      
+
       def show
         redirect_to( :action => :edit )
       end
-      
+
       def clone
         @new = @banner_box.duplicate
 
@@ -21,35 +21,36 @@ module Spree
 
         respond_with(@new) { |format| format.html { redirect_to edit_admin_banner_box_url(@new) } }
       end
-      
+
       protected
       def find_resource
         Spree::BannerBox.find(params[:id])
       end
-      
+
       def location_after_save
         edit_admin_banner_box_url(@banner_box)
       end
-      
+
       def collection
         return @collection if @collection.present?
         params[:q] ||= {}
         params[:q][:s] ||= "created_at desc"
-        params[:category] ||= "compra"
-        params[:q][:enabled_eq] = params[:enabled] if params[:enabled]
-      
-        params[:q][:category_in] = case params[:category]
-          when 'compra' then %w[mainbanner minibanner1 minibanner2]
-          when 'narrow' then %w[narrowbanner cupombanner cupombannermobile]
-          when 'landing' then %w[radio_landing apae_landing]
-          when 'marcas' then %w[brandbanner]
-          when 'logos' then %w[homelogo2]
-          when 'lateral' then %w[sidebanner]
-          else %w[params[:category]]
-        end
-              
+        params[:page] ||= 1
+        # params[:category] ||= "compra"
+        # params[:q][:enabled_eq] = params[:enabled] if params[:enabled]
+
+        # params[:q][:category_in] = case params[:category]
+        #   when 'compra' then %w[mainbanner minibanner1 minibanner2]
+        #   when 'narrow' then %w[narrowbanner cupombanner cupombannermobile]
+        #   when 'landing' then %w[radio_landing apae_landing]
+        #   when 'marcas' then %w[brandbanner]
+        #   when 'logos' then %w[homelogo2]
+        #   when 'lateral' then %w[sidebanner]
+        #   else %w[params[:category]]
+        # end
+
         @search = super.ransack(params[:q])
-        @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
+        @collection = @search.result.page(params[:page]).per(15)
       end
     end
   end
--- app/models/spree/banner_box.rb
+++ app/models/spree/banner_box.rb
@@ -9,27 +9,27 @@ module Spree
     # save the w,h of the original image (from which others can be calculated)
     # we need to look at the write-queue for images which have not been saved yet
     after_post_process :find_dimensions
-    
+
     validates_presence_of :category
     validates_attachment_presence :attachment
     validates_attachment_content_type :attachment, :content_type => ['image/jpeg', 'image/png', 'image/gif', 'image/jpg', 'image/x-png', 'image/pjpeg'], :message => I18n.t(:images_only)
-    
+
     # scope :enable, lambda { |category| {:conditions => {:enabled => true, :category => category}} }
     scope :enable, -> (category) { where(:enabled => true, :category => category) }
-      
+
     # Load user defined paperclip settings
     Spree::BannerBox.attachment_definitions[:attachment][:styles] = ActiveSupport::JSON.decode(Spree::Config[:banner_styles]).symbolize_keys!
     Spree::BannerBox.attachment_definitions[:attachment][:path] = Spree::Config[:banner_path]
     Spree::BannerBox.attachment_definitions[:attachment][:url] = Spree::Config[:banner_url]
     Spree::BannerBox.attachment_definitions[:attachment][:default_url] = Spree::Config[:banner_default_url]
     Spree::BannerBox.attachment_definitions[:attachment][:default_style] = Spree::Config[:banner_default_style]
-        
+
     def initialize(*args)
       super(*args)
       last_banner = BannerBox.last
       self.position = last_banner ? last_banner.position + 1 : 0
     end
-    
+
     # for adding banner_boxes which are closely related to existing ones
     # define "duplicate_extra" for site-specific actions, eg for additional fields
     def duplicate
@@ -44,7 +44,7 @@ module Spree
       p.save!
       p
     end
-    
+
     def find_dimensions
       temporary = attachment.queued_for_write[:original]
       filename = temporary.path unless temporary.nil?
