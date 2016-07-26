586a5f846ac5bdda97e059f6d6ce6c9f9e2fae53
Denise Fugihara <denise@retroca.com.br>
2016-07-26 16:19:38 -0300

    fix banner attachment cofiguration

--- app/models/spree/banner_box.rb
+++ app/models/spree/banner_box.rb
@@ -1,10 +1,24 @@
 module Spree
   class BannerBox < ActiveRecord::Base
+    # has_attached_file :attachment,
+    #                     :path => ":rails_root/public/spree/banners/:id/:style_:basename.:extension",
+    #                     :styles => { :mini => "80x80#", :small => "120x120#" },
+    #                     :s3_credentials => "#{Rails.root}/config/aws.yml",
+    #                     :convert_options => { :all => '-strip -auto-orient' }
     has_attached_file :attachment,
-                        :path => ":rails_root/public/spree/banners/:id/:style_:basename.:extension",
-                        :styles => { :mini => "80x80#", :small => "120x120#" },
-                        :s3_credentials => "#{Rails.root}/config/aws.yml",
-                        :convert_options => { :all => '-strip -auto-orient' }
+                  s3_credentials: { access_key_id:  ENV['S3_ACCESS_KEY'], secret_access_key: ENV['S3_SECRET'], bucket: ENV['S3_BUCKET'] },
+                  storage: :s3,
+                  s3_headers: { "Cache-Control" => 1.year.from_now.httpdate },
+                  s3_protocol:  "https",
+                  s3_region: ENV['S3_REGION'],
+                  bucket: ENV['S3_BUCKET'],
+                  styles: { mini:'192x64#', default:'1000x135>', logo:'250x71#', minibanner:'269x196#', banner_marca: '327x260#', banners_slider_home:'1362x440#', banners_home:'333x143#', banners_marcas:'1000x100#', banner_cupom:'1000x60#', banner_cupom_mobile:'260x57#' },
+                  default_style: :product,
+                  url: ":s3_alias_url",
+                  path: "/banners/:id/:style/:basename.:extension",
+                  default_url:  "/banners/:id/:style/:basename.:extension",
+                  s3_host_alias: Proc.new {|attachment| "assets#{attachment.instance.id % 4}.retroca.com.br" },
+                  convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
 
     # save the w,h of the original image (from which others can be calculated)
     # we need to look at the write-queue for images which have not been saved yet
