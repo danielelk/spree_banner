9c6a9c862dc716668ab02af020d94df6d7b8f5a1
Denise Fugihara <denise@retroca.com.br>
2016-07-29 21:03:39 -0300

    Fix banner products

--- app/models/spree/banner_box.rb
+++ app/models/spree/banner_box.rb
@@ -5,20 +5,21 @@ module Spree
     #                     :styles => { :mini => "80x80#", :small => "120x120#" },
     #                     :s3_credentials => "#{Rails.root}/config/aws.yml",
     #                     :convert_options => { :all => '-strip -auto-orient' }
-    has_attached_file :attachment,
-                  s3_credentials: { access_key_id:  ENV['S3_ACCESS_KEY'], secret_access_key: ENV['S3_SECRET'], bucket: ENV['S3_BUCKET'] },
-                  storage: :s3,
-                  s3_headers: { "Cache-Control" => 1.year.from_now.httpdate },
-                  s3_protocol:  "https",
-                  s3_region: ENV['S3_REGION'],
-                  bucket: ENV['S3_BUCKET'],
-                  styles: { mini:'192x64#', default:'1000x135>', logo:'250x71#', minibanner:'269x196#', banner_marca: '327x260#', banners_slider_home:'1362x440#', banners_home:'333x143#', banners_marcas:'1000x100#', banner_cupom:'1000x60#', banner_cupom_mobile:'260x57#' },
-                  default_style: :product,
-                  url: ":s3_alias_url",
-                  path: "/banners/:id/:style/:basename.:extension",
-                  default_url:  "/banners/:id/:style/:basename.:extension",
-                  s3_host_alias: Proc.new {|attachment| "assets#{attachment.instance.id % 4}.retroca.com.br" },
-                  convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
+
+        has_attached_file :attachment,
+                      s3_credentials: { access_key_id:  ENV['S3_ACCESS_KEY'], secret_access_key: ENV['S3_SECRET'], bucket: ENV['S3_BUCKET'] },
+                      storage: :s3,
+                      s3_headers: { "Cache-Control" => 1.year.from_now.httpdate },
+                      s3_protocol:  "https",
+                      s3_region: ENV['S3_REGION'],
+                      bucket: ENV['S3_BUCKET'],
+                      styles: { mini:'192x64#', default:'1000x135>', logo:'250x71#', minibanner:'269x196#', banner_marca: '327x260#', banners_slider_home:'1362x440#', banners_home:'333x143#', banners_marcas:'1000x100#', banner_cupom:'1000x60#', banner_cupom_mobile:'260x57#' },
+                      default_style: :default,
+                      url: ":s3_alias_url",
+                      path: "/banners/:id/:style/:basename.:extension",
+                      default_url:  "/banners/:id/:style/:basename.:extension",
+                      s3_host_alias: Proc.new {|attachment| "assets#{attachment.instance.id % 4}.retroca.com.br" },
+                      convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
 
     # save the w,h of the original image (from which others can be calculated)
     # we need to look at the write-queue for images which have not been saved yet
@@ -32,11 +33,11 @@ module Spree
     scope :enable, -> (category) { where(:enabled => true, :category => category) }
 
     # Load user defined paperclip settings
-    Spree::BannerBox.attachment_definitions[:attachment][:styles] = ActiveSupport::JSON.decode(Spree::Config[:banner_styles]).symbolize_keys!
-    Spree::BannerBox.attachment_definitions[:attachment][:path] = Spree::Config[:banner_path]
-    Spree::BannerBox.attachment_definitions[:attachment][:url] = Spree::Config[:banner_url]
-    Spree::BannerBox.attachment_definitions[:attachment][:default_url] = Spree::Config[:banner_default_url]
-    Spree::BannerBox.attachment_definitions[:attachment][:default_style] = Spree::Config[:banner_default_style]
+    # Spree::BannerBox.attachment_definitions[:attachment][:styles] = ActiveSupport::JSON.decode(Spree::Config[:banner_styles]).symbolize_keys!
+    # Spree::BannerBox.attachment_definitions[:attachment][:path] = Spree::Config[:banner_path]
+    # Spree::BannerBox.attachment_definitions[:attachment][:url] = Spree::Config[:banner_url]
+    # Spree::BannerBox.attachment_definitions[:attachment][:default_url] = Spree::Config[:banner_default_url]
+    # Spree::BannerBox.attachment_definitions[:attachment][:default_style] = Spree::Config[:banner_default_style]
 
     def initialize(*args)
       super(*args)
