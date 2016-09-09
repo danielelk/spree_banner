1d6f460e5a77bdd45a411e302bf742fcab3a60e8
Denise Fugihara <denise@retroca.com.br>
2016-09-09 16:27:57 -0300

    change menu icon

--- app/overrides/banner_admin_tab.rb
+++ app/overrides/banner_admin_tab.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                       :name => "banner_box_admin_tab",
                       :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                      :text => "<ul class='nav nav-sidebar'><%= tab t(:banner_boxes), url: admin_banner_boxes_url, icon: 'user' %></ul>")
+                      :text => "<ul class='nav nav-sidebar'><%= tab t(:banner_boxes), url: admin_banner_boxes_url, icon: 'film' %></ul>")
 
 Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_configuration",
                       :name => "add_banner_box_settings",
